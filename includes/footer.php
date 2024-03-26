390b6be20851e5d489654d777a912fb484e54fea
Maaaryroooose <113369791+Maaaryroooose@users.noreply.github.com>
2024-03-25 19:33:57 -0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,75 @@
+
+<div class="footer-bg">
+  <div class="container ">
+  <footer class="text-white text-lg-start">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row mt-4 margin-button40">
+        <!--Grid column-->
+         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+           <a href="index.php"> <img style="width:230px;" src="images/logo.png" alt="Christmas Tree Delivered"></a>
+        </div>
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h3 class="text-uppercase mb-4">About company</h3>
+        <hr>
+          <p>
+           Cultured Balsam Fir Christmas trees from Nova Scotia. Delivery to your door in Lethbridge, anywhere on HWY-3 from Coaldale-to-Coleman, and routes north to Claresholm. CTD is community driven, partnering with non-profit organizations every year.
+          </p>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h3 class="text-uppercase mb-4 pb-1">Information</h3>
+            <hr>
+          <ul class="fa-ul" style="margin-left: 1.65em;">
+            <li class="mb-3">
+              <span class="fa-li"><i class="glyphicon glyphicon-map-marker"></i></span><span class="ms-2">Claresholm, Alberta, Canada</span>
+            </li>
+            <li class="mb-3">
+              <span class="fa-li"><i class="glyphicon glyphicon-envelope"></i></span><span class="ms-2">bradystrees@gmail.com</span>
+            </li>
+            <li class="mb-3">
+              <span class="fa-li"><i class="glyphicon glyphicon-earphone"></i></span><span class="ms-2">+ (587) 394-5130</span>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h3 class="text-uppercase mb-4">Menu</h3>
+<hr>
+          <ul class="list-unstyled mb-0">
+            <li class="mb-1">
+              <a href="#!" >Home</a>
+            </li>
+            <li class="mb-1">
+             <a href="#!" >About Us</a>
+            </li>
+            <li class="mb-1">
+              <a href="#!" >Products</a>
+            </li>
+            <li>
+             <a href="#!" >Gallery</a>
+            </li>
+            <li>
+              <a href="#!" >Career</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+     
+    </div>
+    <!-- Grid container -->
+  </footer>
+        </div>
+</div>
+<!-- End of .container -->
+  <div class="text-center cp" >
+      © 2023 Copyright: Christmas Tree Delivered By Global Fusion
+    </div>
